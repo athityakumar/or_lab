822058c7bc47aeb6e04b1d96492cfaa599fa0a9c
athityakumar <athityakumar@gmail.com>
2017-02-28 15:34:30 +0530

    Deployed by bash : Adds more lazy shell commands

--- or_lab.sh
+++ or_lab.sh
@@ -1,12 +1,34 @@
 # Hack to run c files with 1 command like Python / Ruby
-function c {
+function or_c {
 gcc $1 -o $1.out
 ./$1.out
+ls
 }
 
-# Hack to deploy all files to git with 1 command.
-function deploy {
+# Hack to push all files to git with 1 command.
+function or_push {
+cd ~/or_lab/
 git add -A
-git commit -a -m "Deployed by deploy.sh : $1"
+git commit -a -m "Deployed by bash : $1"
 git push origin master
+ls
+}
+
+# Hack to pull all files to git with 1 command. Assumes no merge conflictss. Dies otherwise.
+function or_pull {
+cd ~/or_lab/
+git add -A
+git stash
+git pull origin master
+}
+
+# Too lazy to type 6 extra characters
+function or_cd {
+cd ~/or_lab/
+}
+
+# Hack : cd = cd + ls
+function cd {
+builtin cd $1
+ls
 }

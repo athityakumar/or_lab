f58a940270ebdd0f060e463aaad900e6bad6b53e
athityakumar <athityakumar@gmail.com>
2017-02-28 15:59:30 +0530

    Deployed by bash : Adds bashrc.sh sample code

---
+++ bashrc.sh
@@ -0,0 +1,5 @@
+# Copy paste this code into ~/.bashrc file with proper repo path. This is just an example path.
+# IMPORTANT NOTE : Don't change the variable from "OR_LAB_REPO_PATH" to anything else.
+
+export OR_LAB_REPO_PATH=~/or_lab
+source $OR_LAB_REPO_PATH/or_lab.sh
--- or_lab.sh
+++ or_lab.sh
@@ -7,7 +7,7 @@ ls
 
 # Hack to push all files to git with 1 command.
 function or_push {
-cd ~/or_lab/
+cd $OR_LAB_REPO_PATH
 git add -A
 git commit -a -m "Deployed by bash : $1"
 git push origin master
@@ -16,7 +16,7 @@ ls
 
 # Hack to pull all files to git with 1 command. Assumes no merge conflictss. Dies otherwise.
 function or_pull {
-cd ~/or_lab/
+cd $OR_LAB_REPO_PATH
 git add -A
 git stash
 git pull origin master
@@ -24,7 +24,7 @@ git pull origin master
 
 # Too lazy to type 6 extra characters
 function or_cd {
-cd ~/or_lab/
+cd $OR_LAB_REPO_PATH
 }
 
 # Hack : cd = cd + ls
